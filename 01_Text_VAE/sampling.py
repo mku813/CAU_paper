7e7117afd181a6e4b4672ef134ef0d1bb621ffa1
mku813 <m-wook@nate.com>
2021-09-07 19:43:57 +0900

    KW_test

---
+++ 01_Text_VAE/sampling.py
@@ -0,0 +1,7 @@
+# https://github.com/ryokamoi/original_textvae
+
+import numpy
+import torch
+
+from train import log_and_print
+from vae import VAE
--- 01_Text_VAE/test.py
+++ 01_Text_VAE/test.py
@@ -1,1 +1,4 @@
+import torch
+import numpy as np
 
+torch.sort(torch.tensor([4,2,3,1]), descending=True)
