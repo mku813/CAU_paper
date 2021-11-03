fa2609a70a421f3195913e94789039756b522f98
mku813 <m-wook@nate.com>
2021-11-03 21:18:05 +0900

    create dataset folder

---
+++ cold_posterior/dataset/data.py
@@ -0,0 +1,11 @@
+import torch
+import torchvision
+
+torchvision.datasets.CIFAR10
+
+
+def load_cifar10(split, with_info=False, data_aug=True, subsample_n=0):
+    """
+    Args:
+        split : 
+    """
