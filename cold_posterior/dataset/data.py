791b01f0285c8e98ea92969be3fdf91c55f03798
mku813 <m-wook@nate.com>
2021-11-03 21:36:58 +0900

    data.py back up

--- cold_posterior/dataset/data.py
+++ cold_posterior/dataset/data.py
@@ -1,11 +1,25 @@
 import torch
-import torchvision
+from torchvision import datasets
+import torchvision.transforms as transforms
+from torch.utils.data import Dataset, DataLoader, dataloader
 
-torchvision.datasets.CIFAR10
+import matplotlib.pyplot as plt
+import numpy as np
 
-
-def load_cifar10(split, with_info=False, data_aug=True, subsample_n=0):
+def load_cifar10(batch_size=4):
     """
     Args:
         split : 
     """
+    transform = transforms.Compose(
+        [transforms.ToTensor(), transforms.Normalize((0.5, 0.5, 0.5), (0.5, 0.5, 0.5))]
+    )
+    
+    trainset = datasets.CIFAR10(root='./dataset', train=True, download=True, transform=transform)
+    trainloader = DataLoader(trainset, batch_size=batch_size, shuffle=True, num_workers=2)
+    
+    testset = datasets.CIFAR10(root='./dataset', train=False, download=True, transform=transform)
+    testloader = DataLoader(testset, batch_size=batch_size, shuffle=False, num_workers=2)
+    
+    classes = ('plane', 'car', 'bird', 'cat', 'deer', 'dog', 'frog', 'horse', 'ship', 'truck')
+
